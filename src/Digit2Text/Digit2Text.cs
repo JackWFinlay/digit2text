217b58bbc5d4898f88c305d54e709af175ceca75
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-09-25 21:27:45 +1300

    Set up project.

---
+++ src/Digit2Text/Digit2Text.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Digit2Text
+{
+    public class Digit2Text
+    {
+        private long _digits;
+        public Digit2Text(){
+
+        }
+
+        public long Digits { get => _digits; set => _digits = value; }
+        public string Convert(Int64 digits)
+        {
+            this.Digits = digits;
+            throw new NotImplementedException("Not yet implemented.");
+        }
+    }
+}
---
+++ test/Digit2Text.Tests/Digit2TextTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using Digit2Text;
+
+namespace Digit2Text.Tests
+{
+    public class Digit2TextTests
+    {
+        private readonly Digit2Text _digit2Text;
+
+        public Digit2TextTests()
+        {
+            _digit2Text = new Digit2Text();
+        }
+
+        [Fact]
+        public void ReturnOneGiven1()
+        {
+            var result = _digit2Text.Convert(1);
+
+            Assert.Equal("One", result);
+        }
+    }
+}
